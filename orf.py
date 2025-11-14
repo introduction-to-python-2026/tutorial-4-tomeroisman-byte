b3e5fc5cf081322d03827d4440aa7f45cc53927b
sleepy_tomer <tomeroisman@gmail.com>
2025-11-14 15:13:32 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -1,31 +1,29 @@
 def find_all_starts(dna):
-    start = []
+    starts = []
     for l in range(len(dna)-2):
         if dna[l: l+3] == "ATG":
             starts.append(i)
     return start
 
 def find_first_in_register_stop(dna):
-    stop_codon = []
-    for i in range(len(dna)-2):
-        if dna[i,i+3] in ("TAG", "TGA", "TAA"):
-            stop_codon.append(i)
-    return stop_codons
-
+    for i in range(0, len(dna) - 2, 3):
+        if dna[i:i+3] in ("TAA", "TAG", "TGA"):
+            return i
+    return None
 
 def all_orfs_range(dna):
-    start = find_all_starts(dna)
+    stars = find_all_starts(dna)
     orf_ranges = []
-    for s in start:
+    for s in starts:
         end = find_first_in_register_stop(dna[s:])
-        if end != -1:
+        if end is not None:
             orf_ranges.append((s,s+end))
     return orf_ranges
 
 def longest_orf(dna):
     longest = ""
     for orf_range in all_orfs_range(dna.upper()):
-        orf = dna[orf_range[0]: orf_range[1]]
+        orf = dna[start:stop] 
         if len(orf) > len(longest):
             longest = orf
     return longest
