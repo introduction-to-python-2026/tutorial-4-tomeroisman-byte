2418f2f4f43cf12975b9d16d85edec91cd6d2582
sleepy_tomer <tomeroisman@gmail.com>
2025-11-14 16:55:40 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -1,32 +1,34 @@
-def find_all_starts(dna):
+from typing import List, Optional, Tuple
+
+def find_all_starts(dna: str) -> List[int]:
+    dna = dna.upper()
     starts = []
-    for _1 in range(len(dna)-2):
-        if dna[_1: _1+3] == "ATG":
-            starts.append(_1)
+    for pos in range(len(dna) - 2):
+        if dna[pos:pos + 3] == "ATG":
+            starts.append(pos)
     return starts
 
-def find_first_in_register_stop(dna):
-    for _2 in range(0, len(dna) - 2, 3):
-        if dna[_2:_2+3] in ("TAA", "TAG", "TGA"):
-            return _2
+def find_first_in_register_stop(dna: str) -> Optional[int]:
+    dna = dna.upper()
+    for i in range(0, len(dna) - 2, 3):
+        if dna[i:i+3] in ("TAA", "TAG", "TGA"):
+            return i
     return None
 
-def all_orfs_range(dna):
+def all_orfs_range(dna: str) -> List[Tuple[int, int]]:
     starts = find_all_starts(dna)
     orf_ranges = []
     for start in starts:
         end = find_first_in_register_stop(dna[start:])
         if end is not None:
-            orf_ranges.append((start,start + end))
+            orf_ranges.append((start, start + end + 3))
     return orf_ranges
 
-def longest_orf(dna):
+def longest_orf(dna: str) -> str:
     dna = dna.upper()
     longest = ""
     for start, stop in all_orfs_range(dna):
-        orf = dna[start:stop]   
+        orf = dna[start:stop]
         if len(orf) > len(longest):
             longest = orf
     return longest
-
-
