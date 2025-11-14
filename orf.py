915e94ac7a484777f946b3a7eb7837ce8563db75
sleepy_tomer <tomeroisman@gmail.com>
2025-11-14 16:58:35 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -1,7 +1,6 @@
 from typing import List, Optional, Tuple
 
 def find_all_starts(dna: str) -> List[int]:
-    dna = dna.upper()
     starts = []
     for pos in range(len(dna) - 2):
         if dna[pos:pos + 3] == "ATG":
@@ -9,19 +8,19 @@ def find_all_starts(dna: str) -> List[int]:
     return starts
 
 def find_first_in_register_stop(dna: str) -> Optional[int]:
-    dna = dna.upper()
     for i in range(0, len(dna) - 2, 3):
         if dna[i:i+3] in ("TAA", "TAG", "TGA"):
             return i
     return None
 
 def all_orfs_range(dna: str) -> List[Tuple[int, int]]:
+    dna = dna.upper()
     starts = find_all_starts(dna)
     orf_ranges = []
     for start in starts:
         end = find_first_in_register_stop(dna[start:])
         if end is not None:
-            orf_ranges.append((start, start + end + 3))
+            orf_ranges.append((start, start + end))
     return orf_ranges
 
 def longest_orf(dna: str) -> str:
