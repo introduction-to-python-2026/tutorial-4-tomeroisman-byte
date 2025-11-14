be5bf2a43d7673b0caacf466ceb979b10eb8408f
sleepy_tomer <tomeroisman@gmail.com>
2025-11-14 15:07:32 +0200

    Add ORF detection functions in orf.py
    Implemented functions to find all start codons, stop codons, and determine ORF ranges.

--- orf.py
+++ orf.py
@@ -1,18 +1,34 @@
 def find_all_starts(dna):
-    pass # Replace the `pass` with your code
-
+    start = []
+    for l in range(len(dna)-2):
+        if dna[l: l+3] == "ATG":
+            starts.append(i)
+    return start
 
 def find_first_in_register_stop(dna):
-    pass # Replace the `pass` with your code
+    stop_codon = []
+    for i in range(len(dna)-2):
+        if dna[i,i+3] in ("TAG", "TGA", "TAA"):
+            stop_codon.append(i)
+    return stop_codons
 
 
 def all_orfs_range(dna):
-    pass # Replace the `pass` with your code
-
+    start = find_all_starts(dna)
+    orf_ranges = []
+    for s in start:
+        end = find_first_in_register_stop(dna[s:])
+        if end != -1:
+            orf_ranges.append((s,s+end))
+    return orf_ranges
 
 def longest_orf(dna):
-    pass # Replace the `pass` with your code
-
+    longest = ""
+    for orf_range in all_orfs_range(dna.upper()):
+        orf = dna[orf_range[0]: orf_range[1]]
+        if len(orf) > len(longest):
+            longest = orf
+    return longest
 
 
 
